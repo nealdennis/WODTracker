6030b12253973e54f17282357791c0532f9b9d62
Neal Dennis <neal.dennis1@gmail.com>
2022-03-02 15:31:44 +1100

    Added connection to postgres and random workout selection

--- src/main/java/com/nealdennis/wodtracker/wodAPI.java
+++ src/main/java/com/nealdennis/wodtracker/wodAPI.java
@@ -1,14 +1,14 @@
 package com.nealdennis.wodtracker;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.Random;
 
 public class wodAPI {
     String wodName;
+    Connection con = null;
+    Statement stmt = null;
 
    public wodAPI() {
-       setWodName("Annie");
        connectToDB();
    }
 
@@ -16,18 +16,44 @@ public class wodAPI {
        wodName = inName;
    }
 
-    public String getWOD() {
+   public String getWOD() {
         return wodName;
     }
 
     private void connectToDB() {
-//       try {
-//           registerDriver();
-//       }
+       try {
+           String psqlUrl = "jdbc:postgresql://localhost:5432/wodtracker";
+           con = DriverManager.getConnection(psqlUrl, "postgres", "toolkit");
 
-        //Getting the connection
-//        String mysqlUrl = "jdbc:mysql://localhost/mydatabase";
-//        Connection con = DriverManager.getConnection(mysqlUrl, "root", "password");
-//        System.out.println("Connection established: "+con);
+           stmt = con.createStatement();
+
+           ResultSet maxSet = stmt.executeQuery( "select MAX(wod_id) from public.\"wod\";" );
+           maxSet.next();
+           int max = maxSet.getInt("max");
+           Random r = new Random();
+           int randomInt = r.nextInt(max) + 1;
+
+           ResultSet rs = stmt.executeQuery( "select * from public.\"wod\" where wod_id = " + randomInt + ";" );
+
+           while (rs.next()) {
+
+               String name = rs.getString("name");
+               String description = rs.getString("description");
+
+               if (name != null) {
+                   System.out.println(name);
+                   System.out.print(description);
+                   System.out.println();
+               }
+           }
+
+           rs.close();
+           stmt.close();
+           con.close();
+
+       } catch (SQLException e) {
+           e.printStackTrace();
+       }
+
     }
 }
